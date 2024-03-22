fb9d123995898eb4812a5bffe756ce9ac9a8e0a4
Pat <pat@skal.ski>
2024-03-22 16:39:27 -0400

    Complete Hangman with pictures

--- 7/Hangman.py
+++ 7/Hangman.py
@@ -1,6 +1,62 @@
 
 
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 guesses = []
 goal = input("What is the target word?")
@@ -11,7 +67,7 @@ for letter in goal_parts:
     output += ['_']
 print (output)
 
-while lives >= 0:
+while lives > 0:
     guess = input ("What do letter do you guess?")
     print (guess)
     print (goal_parts)
@@ -28,4 +84,8 @@ while lives >= 0:
     if output == goal_parts:
         print ("You win")
         break
+    print(stages[lives])
 
+if lives == 0:
+    print ("You're dead")
+    print (f"Goal was {goal}")
