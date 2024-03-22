b01eae043006b1c13daaf7e631e7b54fff33e75c
Pat <pat@skal.ski>
2024-03-22 16:30:25 -0400

    Hangman logic, no pictures

--- 7/Hangman.py
+++ 7/Hangman.py
@@ -6,6 +6,10 @@ guesses = []
 goal = input("What is the target word?")
 goal_parts = list(goal)
 lives = 6
+output = []
+for letter in goal_parts:
+    output += ['_']
+print (output)
 
 while lives >= 0:
     guess = input ("What do letter do you guess?")
@@ -14,7 +18,14 @@ while lives >= 0:
     if guess in goal_parts:
         guesses += guess 
         print("correct")
-    else: 
+    if guess not in goal_parts: 
         lives -= 1
         print (f"incorrect, you have {lives} lives remaining.")
+    for letter in range(len(goal_parts)):
+        if guess == goal_parts[letter]:
+            output[letter]=guess
+    print(output)
+    if output == goal_parts:
+        print ("You win")
+        break
 
