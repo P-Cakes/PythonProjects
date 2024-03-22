2373c44706b8e2566f5530dc25f3465fb89329df
Pat <pat@skal.ski>
2024-03-22 17:53:01 -0400

    Complete caesar cipher

--- 8-caesar cipher/main.py
+++ 8-caesar cipher/main.py
@@ -1,10 +1,11 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+'''
 direction = ''
 while direction not in ('encode','decode'):
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+'''
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
@@ -22,6 +23,25 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
 
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode": 
+        shift_amount *= -1 
+    for letter in start_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            if new_position >= 26:
+                new_position -=25
+            if new_position < 0:
+                new_position +=25
+            end_text += alphabet[new_position]
+        else: end_text += letter  
+    print (f"The {cipher_direction}d text is {end_text}")
+
+
+
+'''
 def encrypt(text,shift):
     new_text =''
     for letter in text:
@@ -46,4 +66,38 @@ if direction == 'encode':
     encrypt(text,shift)
 if direction == 'decode':
     decrypt(text,shift)
+
+'''
+
+
+
+#TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+print(logo)
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+should_continue = True
+while should_continue:
+  direction = ''
+  while direction not in ('encode','decode'):
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+  #Try running the program and entering a shift number of 45.
+  #Hint: Think about how you can use the modulus (%).
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  
+  go_again = ''
+  while go_again not in ('yes','no'):
+      go_again = input("Would you like to go again? 'yes' or 'no' ")
+  if go_again == 'no':
+      should_continue = False
+      print("Goodbye")
+
+
+
+
 
