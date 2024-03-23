0f32bc8299a1e5e01507cac7bf61829a065d269d
Pat <pat@skal.ski>
2024-03-22 23:20:58 -0400

    Complete 9 Silent Auction

--- 9 silent auction/main.py
+++ 9 silent auction/main.py
@@ -3,4 +3,32 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 
+from art import logo
+bids = {}
+more_bidders = True
+print(logo)
 
+def bidding():
+    while more_bidders is True:
+        bidder_name = input("What is your name?:")
+        bidder_bid = int(input("What's your bid?: "))
+        bids.update({bidder_name:bidder_bid})
+        more_bid = input("Is there another bidder? 'yes' or 'no'?")
+        if more_bid == 'yes':
+            clear()
+        elif more_bid == 'no':
+            more_bidders == False
+            return bids
+
+bidding()
+
+highest_bid = 0
+highest_bidder = ''
+
+for bidder in bids:
+    bid_amount= bids[bidder]
+    if bid_amount > highest_bid:
+        highest_bid = bid_amount
+        highest_bidder = bidder 
+
+print(f"The highest bidder is {highest_bidder} winning with a bid of ${highest_bid}.")
