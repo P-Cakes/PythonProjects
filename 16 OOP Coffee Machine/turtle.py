0270f473e491c2ced43044c6c0f531cb0a8021fe
Pat <skalskip@bu.edu>
2024-03-24 19:17:16 -0400

    Complete OOP Coffee Maker

--- 16 OOP Coffee Machine/turtle.py
+++ 16 OOP Coffee Machine/turtle.py
@@ -1,5 +1,6 @@
 import turtle
 
+
 timmy = turtle.Turtle()
 
 print(timmy)
@@ -8,6 +9,7 @@ timmy.color("coral")
 timmy.forward(100)
 
 my_screen = turtle.Screen()
+print(my_screen.canvheight)
 my_screen.exitonclick()
 
-timmy.forward(100)
+ 
