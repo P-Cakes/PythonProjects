778c965db2f1e2e2315ea77a1de8ca7f0fa793bd
Pat <skalskip@bu.edu>
2024-03-26 21:48:53 -0400

    Complete Snake Keyboard Controls

--- 20 Snake Game/main.py
+++ 20 Snake Game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import time
+from snake import Snake
 
 screen= Screen()
 screen.setup(width=600, height=600)
@@ -7,26 +8,22 @@ screen.bgcolor("black")
 screen.tracer(0)
 screen.title("My Snake Game")
 
-starting_positions = [(0,0),(-20,0),(-40,0)]
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position[0],position[1])
-    segments.append(new_segment)
+snake = Snake()
+screen.listen()
+screen.onkey(fun=snake.up, key= 'Up')
+screen.onkey(fun=snake.down, key= 'Down')
+screen.onkey(fun=snake.left, key= 'Left')
+screen.onkey(fun=snake.right, key= 'Right')
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(1)
+    snake.move()
 
-    for seg in range(len(segments)-1 ,0,-1):
-        new_x = segments[seg-1].xcor()
-        new_y = segments[seg-1].ycor()
-        segments[seg].goto(new_x, new_y)
-    segments[0].forward(20)
+
+
+
 
 
 
