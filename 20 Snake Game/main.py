a2a5516f05a1d0eede86f232ae4cf386273659a1
Pat <skalskip@bu.edu>
2024-04-04 12:19:02 -0400

    Update Snake Create HighScore and game reset

--- 20 Snake Game/main.py
+++ 20 Snake Game/main.py
@@ -33,16 +33,20 @@ while game_is_on:
 
     # detect collision with wall.
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        score.game_over()
+        #game_is_on = False
+        #score.game_over()
+        score.reset()
+        snake.reset()
 
     # detect collision with tail.
     # if head collides with any segment in tail:
        # trigger game_over
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            #game_is_on = False
+            #score.game_over
+            score.reset()
+            snake.reset()
 
 
 screen.exitonclick()
