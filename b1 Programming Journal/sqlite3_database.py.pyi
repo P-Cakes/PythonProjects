7039bc227b23fcc3a9d47208274b38518f17f633
Pat <skalskip@bu.edu>
2024-04-09 13:15:02 -0400

    Begin Day 25 Weather

--- b1 Programming Journal/sqlite3_database.py.pyi
+++ b1 Programming Journal/sqlite3_database.py.pyi
@@ -4,5 +4,7 @@ connection = sqlite3.connect("data.db")
 
 def create_table():
     with connection:
+        #sqlite3 actually creates the cursor for us here:
+        #but best practice is to use cursor
         connection.execute("create table entries (content text, date text);")
 
