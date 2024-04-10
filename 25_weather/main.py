37b05c03e11c3595b03af46d3ec47b41ee196284
Pat <skalskip@bu.edu>
2024-04-10 13:44:14 -0400

    Basic Pandas Notes

---
+++ 25_weather/main.py
@@ -0,0 +1,60 @@
+
+# with open ("weather_data.csv", mode = "r") as weather:
+#     data = weather.readlines()
+#
+# print (data)
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append( int(row[1]) )
+#
+# print (temperatures)
+
+import pandas
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
+
+temp_list = (data["temp"]).to_list()
+total_temp = sum(temp_list)
+count_temp = len(temp_list)
+avg_temp = total_temp / count_temp
+print (avg_temp)
+
+
+# print (data["temp"].mean())
+#
+# print (data["temp"].max())
+#
+# print (data.condition)
+
+# you can pull the data where the day is monday
+print (data[data.day == 'Monday'])
+
+# you can pull the data with the equivalent of a subquery
+print (data[data.temp == data.temp.max()])
+
+# you can also specify just certain things.
+print (data.day[data.temp == data.temp.max()])
+
+
+# Create a dataframe from scratch.
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 86]
+}
+
+new_data_frame = pandas.DataFrame(data_dict)
+# And you can export into a csv
+new_data_frame.to_csv("new_data_frame.csv")
+
+
+
+
+
+
