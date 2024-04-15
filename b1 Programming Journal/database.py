a5b5c7d0aa5cfb68764ef973aed29353f07bf640
Pat <skalskip@bu.edu>
2024-04-15 15:10:38 -0400

    Start Nato

--- b1 Programming Journal/database.py
+++ b1 Programming Journal/database.py
@@ -2,16 +2,17 @@
 
 entries = []
 
-def add_entry():
-    entry_content = input("What have you learned today? ")
-    entry_date = input("Enter the date: ")
-
-    entries.append ({"content": entry_content, "date": entry_date })
+# def add_entry():
+#     entry_content = input("What have you learned today? ")
+#     entry_date = input("Enter the date: ")
+#
+#     entries.append ({"content": entry_content, "date": entry_date })
 
 
 def view_entries():
     for entry in entries:
         print (f"{entry['date']}\n{entry['content']}\n\n")
+
 
 
 
--- b1 Programming Journal/sqlite3_database.py.pyi
+++ b1 Programming Journal/sqlite3_database.py.pyi
@@ -8,3 +8,10 @@ def create_table():
         #but best practice is to use cursor
         connection.execute("create table entries (content text, date text);")
 
+def add_entry(entry_content, entry_date):
+    with connection:
+        connection.execute(
+            "INSERT INTO entries VALUES (? , ?);",(entry_content, entry_date)
+        )
+
+
