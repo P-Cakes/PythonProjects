a5c5bf49b2a17448236379ba1ad8b390c5d7dc71
Pat <skalskip@bu.edu>
2024-04-15 19:44:18 -0400

    Start Nato Project

--- 26_list_comprehension/main.py
+++ 26_list_comprehension/main.py
@@ -31,3 +31,36 @@
 # overlap = [num for num in num1s if num in num2s]
 # print (overlap)
 
+# Dictionary Comprehension Syntax
+# new_dict = { new_key:new_value for item in list}
+
+# names = ['Alex','Pat','Caroline','Dave','Angela']
+# import random
+# student_scores = {student:random.randint(1,100) for student in names}
+#
+# print (student_scores)
+#
+# passed_students = {student:score for (student, score) in student_scores.items() if score >= 60 }
+#
+# print (passed_students)
+#
+# sentence = 'How many letters are in each of these words?'
+# result = {word:len(word) for word in sentence.split()}
+# print(result)
+
+# How to iterate over a Pandas DataFrame:
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+# for (key, value) in student_dict.items():
+#     print(value)
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+# print (student_data_frame)
+
+for (index, row) in student_data_frame.iterrows():
+    print (row.student)
