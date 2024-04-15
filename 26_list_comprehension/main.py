88719173cda1899eeb6c515430b435355a5e1185
Pat <skalskip@bu.edu>
2024-04-15 16:11:15 -0400

    List Comprehension Practice Complete

---
+++ 26_list_comprehension/main.py
@@ -0,0 +1,33 @@
+
+# # List Comprehension
+# mylist = [1,2,3]
+# # newlist = [new_item for item in original_list ]
+# newlist = [item + 1 for item in mylist]
+#
+# print(mylist)
+# print(newlist)
+
+
+# name = 'Patrick'
+# letters_list = [letter for letter in name]
+# print(letters_list)
+#
+# # conditional list comprehension
+# new_list = [new_item for item in list if test ]
+
+# #Squared numbers:
+# nums = range(1,10)
+# squared_nums = [num**2 for num in nums]
+# print (squared_nums)
+
+# #filter even numbers
+# nums = range(1,10)
+# even_nums = [num for num in nums if num%2 == 0]
+# print (even_nums)
+
+# # data overlap
+# num1s = range(1,10)
+# num2s = range( 5,15)
+# overlap = [num for num in num1s if num in num2s]
+# print (overlap)
+
