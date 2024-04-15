ba2a6f524bce97ece449f78926114e990900192b
Pat <skalskip@bu.edu>
2024-04-15 16:14:02 -0400

    Apply List Comprehension to 25_States_Game

--- 25_States_Game/main.py
+++ 25_States_Game/main.py
@@ -23,10 +23,11 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title= f"{len(guessed_states)}/50 gotten." , prompt="What's another state's name?")
     print(answer_state)
     if answer_state == 'Exit':
-        states_to_learn = []
-        for state in all_states:
-            if state not in guessed_states:
-                states_to_learn.append(state)
+        # states_to_learn = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         states_to_learn.append(state)
+        states_to_learn = [state for state in all_states if state not in guessed_states ]
         new_data = pandas.DataFrame(states_to_learn)
         new_data.to_csv("states_to_learn.csv")
 
