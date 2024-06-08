72065808ee0c530c2b39f952a81dd066e84edc18
Pat <skalskip@bu.edu>
2024-06-07 23:40:30 -0400

    34 api key fix

--- 34_api/main.py
+++ 34_api/main.py
@@ -2,7 +2,10 @@ import requests
 
 my_lat = 40.7989
 my_long = -74.1886
-api_key = "3f304717f619fda0c7bd8d14fe9e91f9"
+# import object from another file
+api_connect = open("api_key.txt", mode = 'r')
+api_key = api_connect.read()
+
 
 api_url = "https://api.openweathermap.org/data/2.5/forecast"
 api_params = {
@@ -20,3 +23,5 @@ for forecast in data['list']:
     # print ((forecast['dt_txt'],forecast['weather']))
     if forecast['weather'][0]['id'] < 700:
         print(f"Bring an umbrella on {forecast['dt_txt']}.")
+    # else:
+    #     print(forecast)
